dbbf4d6837057424cac10dcb96528d51f3d80b29
Aaron Hauck <aaron.hauck@allstardirectories.com>
2020-01-29 15:47:27 -0800

    uncomment some stuff

--- sentences.js
+++ sentences.js
@@ -223,16 +223,15 @@ in the array below
 */
 function getSentence() {
     let functArray = [
-        // a, b, c,
-        // d, e, f,
-        // g, h, i,
-        // j, k, l,
-        // m, n, o,
-        // p, q, r,
-        // s, t, u,
-        // v, w, x,
-        // y, z
-        j
+        a, b, c,
+        d, e, f,
+        g, h, i,
+        j, k, l,
+        m, n, o,
+        p, q, r,
+        s, t, u,
+        v, w, x,
+        y, z
     ];
 
     functArray = helper.arrayShuffle(functArray);
