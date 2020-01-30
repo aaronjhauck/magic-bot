9a9cf95f7d89a2cbeb2951b096e97d0c5d5fe080
Aaron Hauck <aaron.hauck@allstardirectories.com>
2020-01-30 10:56:00 -0800

    folder restructure

---
+++ tests/tests.js
@@ -0,0 +1,35 @@
+const sent = require("./sentences");
+
+let asyncArray = [];
+let nosyncArray = [];
+
+sent.functArray.forEach( e => {
+    if(e.constructor.name == "AsyncFunction") {
+        asyncArray.push(e);
+    }
+    else {
+        nosyncArray.push(e);
+    }
+});
+
+async function testFunction(funct) {
+    let sentence = "";
+
+    if(funct.constructor.name == "AsyncFunction") {
+        sentence = await funct();
+        return sentence;    
+    } else {
+        return funct();
+    }
+}
+
+nosyncArray.forEach(async (e) => {
+    echo(await testFunction(e));
+});
+
+asyncArray.forEach(async e => {
+    echo(await testFunction(e));
+});
+
+
+function echo(string) { console.log(string); }
