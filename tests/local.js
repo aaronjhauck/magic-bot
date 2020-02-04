083973001e2bbf4ff77f9a9e0db022690b1f805e
Aaron Hauck <aaron.hauck@allstardirectories.com>
2020-02-04 15:00:28 -0800

    fix up test

--- tests/local.js
+++ tests/local.js
@@ -1,33 +1,25 @@
-const sent    = require("../lib/sentences");
-
-function loggr(str, err=false) {
-  let today = new Date();
-  let date =  (today.getMonth() + 1) + '/' + today.getDate() + "/" + today.getFullYear();
-  let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-
-  err ? console.error(`[${date}:${time}] [ERROR] ${str}`) 
-      : console.log(`[${date}:${time}] ${str}`);
-}
+const sent = require("../lib/sentences");
+const log  = require("../lib/helpers");
 
 function MockTweet(str) { console.log(str); }
 
 async function TestIt() {
     let sentence = await sent.getSentence();
 
-    loggr("Begining tweet function...");
-    loggr(`Sentence: ${sentence}.`);
+    log.loggr("Begining tweet function...");
+    log.loggr(`Sentence: ${sentence}.`);
 
     try {
-        MockdTweet(sentence);
-        loggr("Tweeted successfully.");
+        MockTweet(sentence);
+        log.loggr("Tweeted successfully.");
     }
     catch (err) {
-        loggr("Unable to send tweet.", true);
-        loggr(err, true);
+        log.loggr("Unable to send tweet.", true);
+        log.loggr(err, true);
     }
 
     setTimeout(() => {
-      loggr("Testing time...");
+      log.loggr("Testing time...");
     }, 3000);
 }
 
