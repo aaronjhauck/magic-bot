d2811625a2f4af8f6139c1516480436df1ead671
Aaron Hauck <aaron.hauck@allstardirectories.com>
2020-02-04 14:11:21 -0800

    trying out something to output to docker logs while the app is running

--- app.js
+++ app.js
@@ -4,12 +4,32 @@ const twit    = require("twit")
     , sched   = require("node-schedule")
     , sent    = require("./lib/sentences");
 
-async function tweet() {
-	var sentence = await sent.getSentence();
-
+function tweet(sentence) {
 	Twitter.post('statuses/update', { status: sentence }, function(err, data, response) {
 		console.log(data)
 	});
 }
 
- var job = sched.scheduleJob('0 */10 * * *', tweet);
+function now() {
+    let today = new Date();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    return `${date} ${time}`;
+}
+
+var job = sched.scheduleJob('0 */10 * * *', async function () {
+    let sentence = await sent.getSentence();
+
+    console.log(now(), "- Begining tweet function...");
+    console.log(now(), "- Sentence: ${sentence}.");
+    console.log(now(), "- Attepting to tweet...");
+
+    try {
+        tweet(sentence);
+        console.log(now(), "- Tweeted successfully.");
+    }
+    catch (err) {
+        consoel.log(now(), "- Unable to send tweet.");
+        console.error(err);
+    }
+});
