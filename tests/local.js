778f7afbc38bcd7cd39f1cf8bdb5688ebb0ac85f
Aaron Hauck <aaron.hauck@allstardirectories.com>
2020-02-04 14:34:31 -0800

    make app.js log better

--- tests/local.js
+++ tests/local.js
@@ -1,9 +1,34 @@
 const sent    = require("../lib/sentences");
 
-async function print() {
-  var sentence = await sent.getSentence();
+function loggr(str, err=false) {
+  let today = new Date();
+  let date =  (today.getMonth() + 1) + '/' + today.getDate() + "/" + today.getFullYear();
+  let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 
-  console.log(sentence);
+  err ? console.error(`[${date}:${time}] [ERROR] ${str}`) 
+      : console.log(`[${date}:${time}] ${str}`);
 }
 
-print();
+function MockTweet(str) { console.log(str); }
+
+async function TestIt() {
+    let sentence = await sent.getSentence();
+
+    loggr("Begining tweet function...");
+    loggr(`Sentence: ${sentence}.`);
+
+    try {
+        MockdTweet(sentence);
+        loggr("Tweeted successfully.");
+    }
+    catch (err) {
+        loggr("Unable to send tweet.", true);
+        loggr(err, true);
+    }
+
+    setTimeout(() => {
+      loggr("Testing time...");
+    }, 3000);
+}
+
+TestIt();
