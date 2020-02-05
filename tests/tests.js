59386f13ff6d23f152df0a4b3421948e51367d0c
Aaron Hauck <aaron.hauck@allstardirectories.com>
2020-02-05 11:22:51 -0800

    tests that work

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,10 @@
 const sent = require("../lib/sentences");
 const expect = require('expect');
 
+/**
+ * Build indices of async
+ * and non-async functions
+ */
 let sync = [];
 let async = [];
 sent.functArray.forEach( e => {
@@ -10,10 +14,38 @@ sent.functArray.forEach( e => {
     else { sync.push(e); }
 });
 
+/**
+ * -- Tests for async functions --
+ * Test A: The function returns something
+ * Test B: The result contains no undefined variables
+ */
 async.forEach(e => {
-    it(`Testing ${e.name}`, async () => {
-        let result = await e();
 
-        expect(result).toReturn();
+    it(`Testing ${e.name} returns something`, async () => {
+        let result = await e();
+        expect(result).toReturn;
+    })
+
+    it(`Testing ${e.name} contains no undefined values`, async () => {
+        let result = await e();
+        expect(result).not.toMatch(/undefined/);
+    })
+});
+
+/**
+ * -- Tests for non-async functions --
+ * Test A: The function returns something
+ * Test B: The result contains no undefined variables
+ */
+sync.forEach(e => {
+
+    it(`Testing ${e.name} returns something`, () => {
+        let result = e();
+        expect(result).toReturn;
+    })
+
+    it(`Testing ${e.name} contains no undefined values`, () => {
+        let result = e();
+        expect(result).not.toMatch(/undefined/);
     })
 })
