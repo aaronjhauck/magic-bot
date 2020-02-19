d09cf7f50db134598c8172dcd1b2ba7b4d3d7235
Aaron Hauck <aaron.hauck@allstardirectories.com>
2020-02-19 15:00:52 -0800

    ignore keys, add key example

--- resource/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key:"VEANaP8uGwHsZtUACeANP9qQH",
-    consumer_secret:"v1xyrCgOwcdh8xZQhZmVi3mgwLgGf3P2WCx2Ue7Rylh7ofB54b",
-    access_token:"1221933737002205184-Lqo7tpvOnyj97l29YjjKBemruGo50S",
-    access_token_secret:"lAkHXKH9HrZ1Xg6yTI7CD8dOQpKUnfV2cxPkK3YmCeu9U"
-}
---
+++ resources/keys_ex.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:"XXXXXXXXXX",
+    consumer_secret:"XXXXXXXXXX",
+    access_token:"XXXXXXXXXX",
+    access_token_secret:"XXXXXXXXXX"
+}
